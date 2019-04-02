642fe10e9d417267f233de11b2717085819a733a
milley <milley.gm@gmail.com>
2019-04-02 20:30:06 +0800

    use cpp for romanToInt

---
+++ week01/algo/lc13/Solution.cpp
@@ -0,0 +1,43 @@
+/********************************************************************************** 
+* 
+* Given a roman numeral, convert it to an integer.
+* 
+* Input is guaranteed to be within the range from 1 to 3999.
+*               
+**********************************************************************************/
+#include <iostream>
+#include<string>
+#include<unordered_map>
+
+using namespace std;
+
+int romanToInt(string s) {
+    unordered_map<char, int> T = {
+        {'I', 1 },
+        { 'V' , 5 },
+        { 'X' , 10 },
+        { 'L' , 50 },
+        { 'C' , 100 },
+        { 'D' , 500 },
+        { 'M' , 1000 }
+    };
+
+    int sum = T[s.back()];
+    for (int i = s.length() - 2; i >= 0; --i) {
+        if (T[s[i]] < T[s[i + 1]]) {
+            sum -= T[s[i]];
+        } else {
+            sum += T[s[i]];
+        }
+    }
+    return sum;
+}
+
+int main(int argc, char **argv) {
+    string s("XL");
+    if (argc > 1) {
+        s = argv[1];
+    }
+    cout << s << " : " << romanToInt(s) << endl;
+    return 0;
+}
