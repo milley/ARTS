e1759ceef6626211f3ee5b88dbde30376b8cb1f1
milley <milley.gm@gmail.com>
2022-07-09 21:58:27 +0800

    algo for week 51

--- week52/algo/lc328/Solution.py
+++ week52/algo/lc328/Solution.py
@@ -1,15 +1,19 @@
 from typing import Optional
 
 # Definition for singly-linked list.
+
+
 class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
+
+
 class Solution:
     def oddEvenList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         if not head:
             return head
-        
+
         evenHead = head.next
         odd, even = head, evenHead
         while even and even.next:
